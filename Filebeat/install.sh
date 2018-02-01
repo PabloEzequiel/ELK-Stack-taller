4bfde43e7c3ea916da8bf86f18818cc5342705d6
Pabloin <pablo.inchausti.bf@gmail.com>
2018-02-01 08:11:03 -0300

    Ejemplos con Filebeat y logs de apache para ubuntu - backup config

--- Filebeat/install.sh
+++ Filebeat/install.sh
@@ -6,8 +6,8 @@ sudo apt-get install apt-transport-https
 echo "deb https://artifacts.elastic.co/packages/6.x/apt stable main" | sudo tee -a /etc/apt/sources.list.d/elastic-6.x.list
 sudo apt-get update && sudo apt-get install filebeat
 
-
-sudo cp /etc/filebeat/filebeat.yml ./filebeat.yml.orig
+sudo cp mkdir /tmp/filebeat_bak
+sudo cp /etc/filebeat/filebeat.yml ./filebeat.yml.bak /tmp/filebeat_bak/filebeat.yml.bak
 sudo cp ./filebeat.yml /etc/filebeat/filebeat.yml
 
 echo "_______________________________________"
