87cded21f90613ac38e28044ba67903f7d470925
Pabloin <pablo.inchausti.bf@gmail.com>
2018-02-07 18:32:36 -0300

    Sección Zero2Hero: Ejercicios Integradores...

---
+++ Zero2Hero/H01 - Logs Apache y Geoip/Readme.md
@@ -0,0 +1,8 @@
+Este ejercicio es comun a varios lados
+Ahora se hace con Filebeats, pero antes era con Logstash ..
+
+Este ejercicio esta basado en el post:
+
+http://gro.solr.pl/elasticsearch-logstash-kibana-to-geo-identify-our-users/
+https://github.com/solrpl/zerotohero/blob/master/logstash.conf
+
---
+++ Zero2Hero/H01 - Logs Apache y Geoip/run.sh
@@ -0,0 +1,6 @@
+# OSX
+export ELK_STACK_DATA_H01="/Users/pabloinchausti/Desktop/DevOps/code/github/Pabloin/ELK-Stack/Zero2Hero/H01 - Logs Apache y Geoip"
+export ELK_STACK_TALK_HOME=/Users/pabloinchausti/Desktop/DevOps/code/github/Pabloin/ELK-Stack
+
+/usr/local/bin/logstash -f ${ELK_STACK_TALK_HOME}/Zero2Hero/logstash-h01.conf --config.reload.automatic
+
