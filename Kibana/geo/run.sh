9221f55f7f7df48861856309ed5be501f72c278a
Pabloin <pablo.inchausti.bf@gmail.com>
2018-02-15 14:41:56 -0300

    Kibana para las PC Ninjas ....

--- Kibana/geo/run.sh
+++ Kibana/geo/run.sh
@@ -1,9 +1,9 @@
-export ELK_STACK_TALK_HOME=/Users/pabloinchausti/Desktop/DevOps/code/github/Pabloin/ELK-Stack
+# export ELK_STACK_TALK_HOME=/Users/pabloinchausti/Desktop/DevOps/code/github/Pabloin/ELK-Stack
 
-/usr/local/bin/logstash -f ${ELK_STACK_TALK_HOME}/Kibana/geo/logstash-geo.conf --config.reload.automatic
+# /usr/local/bin/logstash -f ${ELK_STACK_TALK_HOME}/Kibana/geo/logstash-geo.conf --config.reload.automatic
 
 
 # UBUNTU
-# export ELK_STACK_TALK_HOME=/home/ninja/github/ELK-Stack
-# /opt/logstash/bin/logstash -f ${ELK_STACK_TALK_HOME}/Kibana/geo/logstash-geo.conf
+export ELK_STACK_TALK_HOME=/home/ninja/github/ELK-Stack
+/opt/logstash/bin/logstash -f ${ELK_STACK_TALK_HOME}/Kibana/geo/logstash-geo.conf
 
