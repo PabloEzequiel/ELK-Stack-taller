8558e4b105ce92d6a33b7b5803f79d79a6333f8d
Pabloin <pablo.inchausti.bf@gmail.com>
2018-02-21 14:19:52 -0300

    Ejemplo 03

--- Zero2Hero/H02 - Twitter/run.sh
+++ Zero2Hero/H02 - Twitter/run.sh
@@ -1,7 +1,11 @@
-# OSX
-export ELK_STACK_DATA_H03="/Users/pabloinchausti/Desktop/DevOps/code/github/Pabloin/ELK-Stack/Zero2Hero/H02 - Twitter"
-export ELK_STACK_TALK_HOME=/Users/pabloinchausti/Desktop/DevOps/code/github/Pabloin/ELK-Stack
+# Ubuntu Ninja Notebook
 
-/usr/local/bin/logstash -f "${ELK_STACK_TALK_HOME}/Zero2Hero//H02 - Twitter"/twitter-logstash.conf --config.reload.automatic
 
+export ELK_STACK_TALK_HOME=/home/ninja/github/ELK-Stack
+
+echo "version de logstash: Necesito (6.2.2) "
+/usr/share/logstash/bin/logstash --version
+
+
+sudo /usr/share/logstash/bin/logstash -f "${ELK_STACK_TALK_HOME}/Zero2Hero/H02 - Twitter"/twitter-logstash.conf 
 
--- Zero2Hero/H03 - Bitcoin/run.sh
+++ Zero2Hero/H03 - Bitcoin/run.sh
@@ -1,5 +1,9 @@
-# OSX
-export ELK_STACK_DATA_H03="/Users/pabloinchausti/Desktop/DevOps/code/github/Pabloin/ELK-Stack/Zero2Hero/H03 - Bitcoin"
-export ELK_STACK_TALK_HOME=/Users/pabloinchausti/Desktop/DevOps/code/github/Pabloin/ELK-Stack
+# Ubuntu Ninja Notebook
 
-/usr/local/bin/logstash -f "${ELK_STACK_TALK_HOME}/Zero2Hero//H03 - Bitcoin"/logstash-H03.conf --config.reload.automatic
+export ELK_STACK_TALK_HOME=/home/ninja/github/ELK-Stack
+
+echo "version de logstash: Necesito (6.2.2) "
+/usr/share/logstash/bin/logstash --version
+
+sudo /usr/share/logstash/bin/logstash -f "${ELK_STACK_TALK_HOME}/Zero2Hero/HH03 - Bitcoin"/logstash-h03.conf 
+
