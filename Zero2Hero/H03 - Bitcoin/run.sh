ca472f8ba165220eae3c6b6c32c1f9357016e0d3
Pabloin <pablo.inchausti.bf@gmail.com>
2018-02-21 14:24:07 -0300

    H03 listo

--- Zero2Hero/H03 - Bitcoin/run.sh
+++ Zero2Hero/H03 - Bitcoin/run.sh
@@ -5,5 +5,5 @@ export ELK_STACK_TALK_HOME=/home/ninja/github/ELK-Stack
 echo "version de logstash: Necesito (6.2.2) "
 /usr/share/logstash/bin/logstash --version
 
-sudo /usr/share/logstash/bin/logstash -f "${ELK_STACK_TALK_HOME}/Zero2Hero/HH03 - Bitcoin"/logstash-h03.conf 
+sudo /usr/share/logstash/bin/logstash -f "${ELK_STACK_TALK_HOME}/Zero2Hero/H03 - Bitcoin"/logstash-h03.conf 
 
